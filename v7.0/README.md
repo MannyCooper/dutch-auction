8da3b7de0526d0cc7fcb277a27ba4afdd8b0d420
异次元de机智君 <an1998821@gmail.com>
2023-04-07 22:58:20 -0700

    Alchemy & Sepolia

--- v7.0/README.md
+++ v7.0/README.md
@@ -1,5 +1,8 @@
 # Sample Hardhat Project
 
+IPFS:
+ipfs://12D3KooWK9dKWwXPzT8WExhD5jCrVYYoHXs4iJiZN4vQA3LXQGSZ
+
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
 Try running some of the following tasks:
--- v7.0/hardhat.config.ts
+++ v7.0/hardhat.config.ts
@@ -1,11 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "solidity-coverage";
+import dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + "/.env" });
+const { ALCHEMY_API_KEY, SEPOLIA_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./frontend/src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY as string],
+    },
   },
 };
 
